5d21122dfa6f46284b325d9a919de5e656f6f082
davidebalice <davide.balice@gmail.com>
2024-08-28 09:08:47 +0200

    update

--- src/components/Meme/presetMeme.js
+++ src/components/Meme/presetMeme.js
@@ -1,4 +1,11 @@
 import baseMeme1 from "../../assets/images/baseMeme1.jpg";
+import baseMeme10 from "../../assets/images/baseMeme10.jpg";
+import baseMeme11 from "../../assets/images/baseMeme11.jpg";
+import baseMeme12 from "../../assets/images/baseMeme12.jpg";
+import baseMeme14 from "../../assets/images/baseMeme14.jpg";
+import baseMeme15 from "../../assets/images/baseMeme15.jpg";
+import baseMeme16 from "../../assets/images/baseMeme16.jpg";
+import baseMeme17 from "../../assets/images/baseMeme17.jpg";
 import baseMeme2 from "../../assets/images/baseMeme2.jpg";
 import baseMeme3 from "../../assets/images/baseMeme3.jpg";
 import baseMeme4 from "../../assets/images/baseMeme4.jpg";
@@ -7,10 +14,14 @@ import baseMeme6 from "../../assets/images/baseMeme6.jpg";
 import baseMeme7 from "../../assets/images/baseMeme7.jpg";
 import baseMeme8 from "../../assets/images/baseMeme8.jpg";
 import baseMeme9 from "../../assets/images/baseMeme9.jpg";
-import baseMeme10 from "../../assets/images/baseMeme10.jpg";
-import baseMeme11 from "../../assets/images/baseMeme11.jpg";
-import baseMeme12 from "../../assets/images/baseMeme12.jpg";
 
+import baseMeme10thumb from "../../assets/images/baseMeme10thumb.jpg";
+import baseMeme11thumb from "../../assets/images/baseMeme11thumb.jpg";
+import baseMeme12thumb from "../../assets/images/baseMeme12thumb.jpg";
+import baseMeme14thumb from "../../assets/images/baseMeme14thumb.jpg";
+import baseMeme15thumb from "../../assets/images/baseMeme15thumb.jpg";
+import baseMeme16thumb from "../../assets/images/baseMeme16thumb.jpg";
+import baseMeme17thumb from "../../assets/images/baseMeme17thumb.jpg";
 import baseMeme1thumb from "../../assets/images/baseMeme1thumb.jpg";
 import baseMeme2thumb from "../../assets/images/baseMeme2thumb.jpg";
 import baseMeme3thumb from "../../assets/images/baseMeme3thumb.jpg";
@@ -20,9 +31,6 @@ import baseMeme6thumb from "../../assets/images/baseMeme6thumb.jpg";
 import baseMeme7thumb from "../../assets/images/baseMeme7thumb.jpg";
 import baseMeme8thumb from "../../assets/images/baseMeme8thumb.jpg";
 import baseMeme9thumb from "../../assets/images/baseMeme9thumb.jpg";
-import baseMeme10thumb from "../../assets/images/baseMeme10thumb.jpg";
-import baseMeme11thumb from "../../assets/images/baseMeme11thumb.jpg";
-import baseMeme12thumb from "../../assets/images/baseMeme12thumb.jpg";
 
 const presetMeme = [
   {
@@ -392,6 +400,206 @@ const presetMeme = [
     border2: 0,
     borderColor2: "#333333",
     alignText2: "center",
+
+    textOutside: false,
+    imageSelected: null,
+  },
+  {
+    id: 14,
+    src: baseMeme14,
+    thumb: baseMeme14thumb,
+    useText: 6,
+
+    text1: "Designers",
+    top1: 4,
+    left1: 1,
+    size1: 1.8,
+    color1: "#000000",
+    border1: 0,
+    borderColor1: "#333333",
+    alignText1: "center",
+
+    text2: "Look, we have a similar idea.",
+    top2: 44,
+    left2: -24,
+    size2: 1,
+    color2: "#333333",
+    border2: 0,
+    borderColor2: "#333333",
+    alignText2: "center",
+
+    text3: "No you stole my idea.",
+    top3: 44,
+    left3: 27,
+    size3: 1,
+    color3: "#333333",
+    border3: 0,
+    borderColor3: "#333333",
+    alignText3: "center",
+
+    text4: "Programmers",
+    top4: 52,
+    left4: 1,
+    size4: 1.8,
+    color4: "#333333",
+    border4: 0,
+    borderColor4: "#333333",
+    alignText4: "center",
+
+    text5: "Man I stole your code.",
+    top5: 93,
+    left5: -25,
+    size5: 1,
+    color5: "#333333",
+    border5: 0,
+    borderColor5: "#333333",
+    alignText5: "center",
+
+    text6: "It's not my code",
+    top6: 93,
+    left6: 27,
+    size6: 1,
+    color6: "#333333",
+    border6: 0,
+    borderColor6: "#333333",
+    alignText6: "center",
+
+    textOutside: false,
+    imageSelected: null,
+  },
+  {
+    id: 15,
+    src: baseMeme15,
+    thumb: baseMeme15thumb,
+    useText: 3,
+
+    text1: "Backend",
+    top1: 4,
+    left1: 1,
+    size1: 1.8,
+    color1: "#ffffff",
+    border1: 0,
+    borderColor1: "#333333",
+    alignText1: "center",
+
+    text2: "Frontend",
+    top2: 36,
+    left2: 1,
+    size2: 1.8,
+    color2: "#ffffff",
+    border2: 0,
+    borderColor2: "#333333",
+    alignText2: "center",
+
+    text3: "Api",
+    top3: 93,
+    left3: 1,
+    size3: 1.8,
+    color3: "#ffffff",
+    border3: 0,
+    borderColor3: "#333333",
+    alignText3: "center",
+
+    textOutside: false,
+    imageSelected: null,
+  },
+
+  {
+    id: 16,
+    src: baseMeme16,
+    thumb: baseMeme16thumb,
+    useText: 3,
+
+    text1: "Backend",
+    top1: 1,
+    left1: -39,
+    size1: 1.8,
+    color1: "#333333",
+    border1: 0,
+    borderColor1: "#333333",
+    alignText1: "center",
+
+    text2: "Frontend",
+    top2: 1,
+    left2: 39,
+    size2: 1.8,
+    color2: "#333333",
+    border2: 0,
+    borderColor2: "#333333",
+    alignText2: "center",
+
+    text3: "Internal server error",
+    top3: 70,
+    left3: 14,
+    size3: 1,
+    color3: "#ff0000",
+    border3: 0,
+    borderColor3: "#333333",
+    alignText3: "center",
+
+    textOutside: false,
+    imageSelected: null,
+  },
+
+  {
+    id: 17,
+    src: baseMeme17,
+    thumb: baseMeme17thumb,
+    useText: 6,
+
+    text1: "Today",
+    top1: 4,
+    left1: -37,
+    size1: 1.8,
+    color1: "#333333",
+    border1: 0,
+    borderColor1: "#333333",
+    alignText1: "center",
+
+    text2: "Programmer",
+    top2: 17,
+    left2: 3,
+    size2: 1.4,
+    color2: "#333333",
+    border2: 0,
+    borderColor2: "#333333",
+    alignText2: "center",
+
+    text3: "Ai",
+    top3: 35,
+    left3: -19,
+    size3: 1.8,
+    color3: "#ffffff",
+    border3: 0,
+    borderColor3: "#333333",
+    alignText3: "center",
+
+    text4: "Tomorrow",
+    top4: 57,
+    left4: -34,
+    size4: 1.8,
+    color4: "#333333",
+    border4: 0,
+    borderColor4: "#333333",
+    alignText4: "center",
+
+    text5: "Programmer",
+    top5: 90,
+    left5: 14,
+    size5: 1,
+    color5: "#333333",
+    border5: 0,
+    borderColor5: "#333333",
+    alignText5: "center",
+
+    text6: "Ai",
+    top6: 66,
+    left6: 23,
+    size6: 3.3,
+    color6: "#ffffff",
+    border6: 0,
+    borderColor6: "#333333",
+    alignText6: "center",
 
     textOutside: false,
     imageSelected: null,
